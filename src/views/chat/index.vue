11d38e5810c6b902cc69ad345e0c665b6e6d1ef7
Equim <sayaka@ekyu.moe>
2023-03-19 12:34:49 +0800

    revert changes, add back delete dialog

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -488,7 +488,16 @@ function handleExport() {
 function handleDelete(index: number) {
   if (loading.value)
     return
-  chatStore.deleteChatByUuid(+uuid, index)
+
+  dialog.warning({
+    title: t('chat.deleteMessage'),
+    content: t('chat.deleteMessageConfirm'),
+    positiveText: t('common.yes'),
+    negativeText: t('common.no'),
+    onPositiveClick: () => {
+      chatStore.deleteChatByUuid(+uuid, index)
+    },
+  })
 }
 
 function handleClear() {
